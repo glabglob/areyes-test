077af30c96cce695e9e31cb687bb6beb18b129fb
Gleb Kononenko <fed.gleb.kononenko@gmail.com>
2023-04-06 14:42:50 +0300

    fix

--- src/store/rootReducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-
-import modalSliceReducer from "./modal/modalSlice";
-
-const rootReducer = combineReducers({
-    modal: modalSliceReducer,
-});
-
-export default rootReducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import rootReducer from './rootReducer';
+
 
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: {}
 });
 
 export type AppDispatch = typeof store.dispatch;
